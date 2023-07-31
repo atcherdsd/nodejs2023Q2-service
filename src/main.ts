d7a98eda34ba9b61609d2066c2ca5e079b103820
atcherdsd <94963661+atcherdsd@users.noreply.github.com>
2023-07-31 09:18:20 +0300

    feat: connect api.yaml

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,22 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as dotenv from 'dotenv';
+import { readFile } from 'fs/promises';
+import { SwaggerModule } from '@nestjs/swagger';
+import { resolve } from 'path';
+import { cwd } from 'process';
+import { parse } from 'yaml';
 
 dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+
+  const yamlFile = resolve(cwd(), 'doc', 'api.yaml');
+  const fileContent = await readFile(yamlFile, 'utf-8');
+  SwaggerModule.setup('doc', app, parse(fileContent));
+
   await app.listen(Number(process.env.PORT) || 4000);
 }
 bootstrap();
